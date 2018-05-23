23bf361e9d298c66c053e0e934429ad92e7504bf
Lucy Wall <lhwall@gmail.com>
2018-05-23 12:49:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items/#{@name}"
+    if req.path == "/items/"
       resp.write @price
     else
       resp.write "Route not found"
