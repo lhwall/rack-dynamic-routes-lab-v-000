dbefe2e7bf50e86fd2f0001918cf6cfa5bf29fc6
Lucy Wall <lhwall@gmail.com>
2018-05-23 12:48:02 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class application
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+
+    if req.path == "/items/#{item.name}"
+      resp.write item.price
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    resp.finish
+  end 
+end 
