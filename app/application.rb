af673327b3d8c304a9268c9f33801a14791144e4
Lucy Wall <lhwall@gmail.com>
2018-05-23 12:56:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items/#{@name}"
+    if req.path.scan(\items\)
       resp.write @price
 
     else
