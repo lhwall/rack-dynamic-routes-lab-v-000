fc7da3188db0bca40cb23a245764ca094975e5a3
Lucy Wall <lhwall@gmail.com>
2018-05-23 12:55:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,8 @@
-require 'pry'
-
 class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-      binding.pry
+
     if req.path == "/items/#{@name}"
       resp.write @price
 
