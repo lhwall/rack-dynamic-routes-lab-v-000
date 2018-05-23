857c5be68064a4257336c33baf878980d357cbc2
Lucy Wall <lhwall@gmail.com>
2018-05-23 13:01:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     if req.path.match(/items/)
      item_name = req.path.split("/items").last
-     item = @@items.find{|s| s.name == item_name}
+     item = @@items.find{|s| s.name == item.name}
       resp.write item.price
     else
       resp.write "Route not found"
