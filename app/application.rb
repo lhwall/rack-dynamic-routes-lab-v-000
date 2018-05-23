76fabdf503381c5254d4cd0aa3009a241f73d55b
Lucy Wall <lhwall@gmail.com>
2018-05-23 13:03:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,12 @@ class Application
     if req.path.match(/items/)
      item_name = req.path.split("/items/").last
      item = @@items.find{|s| s.name == item_name}
+     if !item 
+       resp.write "Item not found"
+       resp.status = 404
+     else 
       resp.write item.price
+    end 
     else
       resp.write "Route not found"
       resp.status = 404
